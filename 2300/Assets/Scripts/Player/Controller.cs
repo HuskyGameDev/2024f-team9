be99b42e46d2dea3cbcbdeb342928a80b6c07257
Conor <ctcantre@mtu.edu>
2024-10-30 20:45:08 -0400

    Finalized rotation and player animations, added crosshair

--- 2300/Assets/Scripts/Player/Controller.cs
+++ 2300/Assets/Scripts/Player/Controller.cs
@@ -12,9 +12,12 @@ public class Controller : MonoBehaviour
     public Camera cam;
     private float hdirection;
     private float vdirection;
-    Vector2 mousePos;
-    Vector2 xRef = new Vector2(.5f, .5f);
-    Vector2 yRef = new Vector2(.5f, -.5f);
+    private float angle;
+    public Vector2 xRef;
+    public Vector2 yRef;
+    private Vector3 mousePos;
+    private Vector3 crossHair;
+    
 
     private enum State {idle, runl, runr, runu, rund};
     private State state = State.idle;
@@ -29,67 +32,53 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 crossHair = cam.ScreenToWorldPoint(Input.mousePosition);
+        MoveCursor(crossHair);
+        float normalizedX = (mousePos.x / Screen.width) * 2 - 1; // Normalize to -1 to 1
+        float normalizedY = (mousePos.y / Screen.height) * 2 - 1; // Normalize to -1 to 1
+        // Calculate angle to face
+        angle = Mathf.Atan2(normalizedY, normalizedX) * Mathf.Rad2Deg;
+        if (angle < 360) {
+            angle += 360;
+        }
+        anim.SetInteger("state", (int)state);
+        AngleState(angle);
+
         hdirection = Input.GetAxisRaw("Horizontal");
         vdirection = Input.GetAxisRaw("Vertical");
-
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector2 deltaX = dotProdX(xRef, mousePos);
-        Vector2 deltaY = dotProdY(yRef, mousePos);
         
-        faceState(deltaX, deltaY);
-
-        anim.SetInteger("state", (int)state);
     }
 
     private void FixedUpdate()
     {
         MovePlayer(hdirection, vdirection);
-        MoveCursor(mousePos);
     }
 
     void MovePlayer(float h, float v) 
     {
          Vector2 targetVelocity = new Vector2(h * speed, v * speed);
          rb.velocity = new Vector2(targetVelocity.x, targetVelocity.y); 
-
     }
 
-    void MoveCursor(Vector2 mousePos)
+    void MoveCursor(Vector3 mousePos)
     {
+        mousePos.z = 0;
         crosshair.transform.position = mousePos;
     }
 
-   
-
-    private void faceState(Vector2 deltaX, Vector2 deltaY)
-    {
-       if (deltaX.x > 0 && deltaY.y > 0)
-        {
-            state = State.runr;
-        }
-        if (deltaX.x < 0 && deltaY.y > 0)
-        {
-            state = State.rund;
-        }
-        if (deltaX.x < 0 && deltaY.y < 0)
-        {
-            state = State.runl;
-        }
-        if (deltaX.x > 0 && deltaY.y < 0)
-        {
+    private void AngleState (float angle) {
+        if (angle < 490 && angle > 400 ) {
             state = State.runu;
         }
-        
+        if ((angle > 490 || angle > 180) && angle < 225) {
+            state = State.runl;
+        }
+        if (angle > 225 && angle < 315) {
+            state = State.rund;
+        }
+        if (angle > 315 && angle < 400) {
+            state = State.runr;
+        }
     }
-
-    private Vector2 dotProdX(Vector2 xRef, Vector2 mousePos) {
-        return new Vector2((xRef.x * mousePos.x),(xRef.y * mousePos.y));
-    }
-
-    private Vector2 dotProdY(Vector2 yRef, Vector2 mousePos) {
-        return new Vector2((yRef.x * mousePos.x),(yRef.y * mousePos.y));
-    }
-
-    
 }
