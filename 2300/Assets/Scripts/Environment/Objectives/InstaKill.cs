69d11483a796fbfbfc68ae8025ed53c592f810cd
ndbrewer <ndbrewer@mtu.edu>
2024-11-17 19:43:59 -0500

    InstaKill, Objective, values, and organization
    InstaKill:
    * implemented the Insta kill objective functionality
    
    Objective:
    * properly handles progress bar functionality
    
    Values:
    * Tweaked some scripts values (generate level, objective, and player/enemy values)
    
    Organized files.

--- 2300/Assets/Scripts/Environment/Objectives/InstaKill.cs
+++ 2300/Assets/Scripts/Environment/Objectives/InstaKill.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class InstaKill : Objective
 {
-    
+
+    [Header("Instant Kill")]
+    public LayerMask enemyLayer;
+    public float explosionRadius = 8;
+    public float explosionVfxEndAfter = 2;
+
+    private ParticleSystem _ps;
+
+    protected new void Awake()
+    {
+        base.Awake();
+        _ps = GetComponentInChildren<ParticleSystem>();
+        _ps?.Stop();
+    }
+
+    private void OnEnable()
+    {
+        progressFinished.AddListener(Explode);
+    }
+
+    private void OnDisable()
+    {
+        progressFinished.RemoveListener(Explode);
+    }
+
+    private void Explode(GameObject go)
+    {
+        StartCoroutine(ExplodeFX());
+        var results = Physics2D.OverlapCircleAll(go.transform.position, explosionRadius, enemyLayer);
+        foreach (var result in results)
+        {
+            result.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator ExplodeFX()
+    {
+        _ps?.Play();
+        yield return new WaitForSeconds(explosionVfxEndAfter);
+        _ps?.Stop();
+    }
 }
--- 2300/Assets/Scripts/Environment/Objectives/Objective.cs
+++ 2300/Assets/Scripts/Environment/Objectives/Objective.cs
@@ -1,25 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Objective : MonoBehaviour
 {
+    [Header("Objective")]
     [SerializeField]
     private Slider progressBar;
-    [Range(0f, 1f), Tooltip("How fast does the Progress Bar Decay")]
+    protected float _progress
+    {
+        get { return progressBar.value; }
+        set
+        {
+            var val = Mathf.Clamp01(value);
+            progressBar.value = val;
+        }
+    }
+    private bool _playerDetected = false;
+
+    [Header("Fill")]
+    public float maxFill = 1, minFill = 0;
+    [Tooltip("How fast does the Progress Bar Fill each second?")]
+    public float fillRate = 0.1f;
+
+    [Header("Decay")]
+    [Tooltip("How fast does the Progress Bar Decay?")]
     public float decayRate = 0.05f;
     [Tooltip("When does the progress bar start decaying?")]
-    public float decayStartAfter = 2;
+    public float decayStartAfter = 3;
+    private float _decayTime;
 
-    public void Start()
+    [Header("Player Detection")]
+    [Tooltip("How close does the player have to be to start the objective?")]
+    public float detectionRange = 2;
+    public LayerMask playerLayer;
+
+    [HideInInspector]
+    public UnityEvent<GameObject> progressFinished;
+    private bool _recharge;
+
+    protected void Awake()
     {
         if (progressBar == null)
         {
             var sl = gameObject.GetComponentInChildren<Slider>();
-            if (!sl)
+            if (!sl) // if no child has this component then look in your children for the component.
             {
-                for(int i = 0; i < transform.childCount; i++)
+                for (int i = 0; i < transform.childCount; i++) // loop over all children and check for slider component in their children
                 {
                     var c = transform.GetChild(i);
                     sl = c.GetComponentInChildren<Slider>();
@@ -30,9 +60,83 @@ public class Objective : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void Start()
     {
-        
+        if (progressBar)
+        {
+            progressBar.maxValue = maxFill;
+            progressBar.minValue = minFill;
+
+            _progress = minFill;
+        }
+        StartCoroutine(decay());
+        StartCoroutine(fill());
+    }
+
+    // Update is called once per fixed framerate frame
+    protected void FixedUpdate()
+    {
+        if (!_playerDetected && _decayTime >= decayStartAfter)
+        {
+            _decay = true;
+            _fill = false;
+        }
+
+        if (Physics2D.OverlapCircle(transform.position, detectionRange, playerLayer)) // if the player is found by the colliders
+        {
+            _fill = true;
+            _decay = false;
+            _playerDetected = true;
+            _decayTime = 0;
+        }
+        else
+        {
+            _fill = false;
+            _playerDetected = false;
+            _decayTime += Time.fixedDeltaTime;
+        }
+
+        if (_recharge)
+        {
+            _fill = false;
+            _decay = false;
+        }
+    }
+
+
+    private bool _decay = false;
+    private IEnumerator decay()
+    {
+        var wait = new WaitForSeconds(1);
+
+        while (true)
+        {
+
+            yield return wait; // wait 1 second before decaying progress bar
+            if (_decay || _recharge)
+                _progress -= decayRate;
+
+            if (_progress <= minFill)
+                _recharge = false;
+        }
+    }
+
+    private bool _fill = false;
+    private IEnumerator fill()
+    {
+        var wait = new WaitForSeconds(1);
+
+        while (true)
+        {
+            yield return wait; // wait 1 second before filling progress bar
+            if (_fill)
+                _progress += fillRate;
+
+            if (_progress >= maxFill)
+            {
+                progressFinished.Invoke(gameObject);
+                _recharge = true;
+            }
+        }
     }
 }
