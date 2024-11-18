9aec3066d4d382e18befcfcb7d05a4b49e33cfcf
Roscoehawk <aarvige@gmail.com>
2024-11-18 17:16:31 -0500

    faster shooting power up and slight bullet adjustments
    first power up of 2 and some fixes

--- 2300/Assets/shooting.cs
+++ 2300/Assets/shooting.cs
@@ -24,8 +24,6 @@ public class shooting : MonoBehaviour
     {
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 rotation = mousePos - transform.position;
-        //float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(0, 0, rotZ);
         if(!canFire){
             timer += Time.deltaTime;
             if(timer > timeBetweenFiring){
