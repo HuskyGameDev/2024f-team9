fae92b61ccdfeb12aeb6678ce5fda71855cf647c
Nick Abraham <nrabraha@mtu.edu>
2024-11-18 21:46:34 -0500

    Adjusted camera follow to work better with portals

--- 2300/Assets/Scripts/Camera/CameraFollow.cs
+++ 2300/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,14 +4,14 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform playerTarget;
 
-    void Start()
-    {
-        
-    }
-
     void FixedUpdate()
     {
         Vector3 playerTargetPos = new Vector3(playerTarget.position.x, playerTarget.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, playerTargetPos, 0.2f);
+
+        if ((playerTargetPos - transform.position).magnitude > 2.0f)
+        {
+            transform.position = playerTargetPos;
+        }
     }
 }
